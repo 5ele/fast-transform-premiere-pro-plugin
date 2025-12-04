b1217a45c6324dd0f6699aba24251b757df30660
5ele <204300212+5ele@users.noreply.github.com>
2025-12-03 20:53:53 -0500

    Update param getter; got opacity values

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+/// <reference path="./types/ppro.d.ts" />
+
 import type React from "react";
 import { useEffect, useState } from "react";
 import { api } from "./api/api";
@@ -6,7 +8,9 @@ import reactLogo from "./assets/react.png";
 import sassLogo from "./assets/sass.png";
 import tsLogo from "./assets/typescript.png";
 import viteLogo from "./assets/vite.png";
+import { getKeyframesFromMotionAndOpacity } from "./components/transfer-keyframes";
 import { premierepro, uxp } from "./globals";
+import { AudioClipTrackItem, VideoClipTrackItem } from "./types/ppro";
 
 declare global {
 	namespace JSX {
@@ -19,65 +23,85 @@ declare global {
 	}
 }
 
+/**
+ * shape:
+ * premierepro > Project > Sequence > Selection > Tracks > Track > Components > Component > Params > Param
+ * Components are "effects"
+ * Params are params like "scale", "position", ...
+ */
+
 export const App = () => {
-	const webviewUI = import.meta.env.VITE_BOLT_WEBVIEW_UI === "true";
+	const [count, setCount] = useState<any>();
+	const handleClick = async () => await getKeyframesFromMotionAndOpacity();
+	const handleSettingsClick = async () => {};
+	// const increment = () => setCount((prev) => prev + 1);
 
-	const [count, setCount] = useState(0);
-	const increment = () => setCount((prev) => prev + 1);
-
-	const hostName = (uxp.host.name as string).toLowerCase();
-
-	//* Call Functions Conditionally by App
-	if (hostName === "premierepro") {
-		console.log("Hello from Premiere Pro", premierepro);
-	}
-
-	//* Or call the unified API object directly and the correct app function will be used
-	const simpleAlert = () => {
-		api.notify("Hello World");
-	};
 	return (
-		<>
-			{!webviewUI ? (
-				<main>
-					<div>
-						<img className="logo-lg" src={boltUxpLogo} alt="" />
-					</div>
-					<div className="stack-icons">
-						<img src={viteLogo} className="logo" alt="" />
-						<span> + </span>
-						<img src={reactLogo} className="logo" alt="" />
-						<span> + </span>
-						<img src={tsLogo} className="logo" alt="" />
-						<span> + </span>
-						<img src={sassLogo} className="logo" alt="" />
-					</div>
-					<div className="button-group">
-						<button onClick={increment}>count is {count}</button>
-						<button onClick={simpleAlert}>Alert</button>
-					</div>
-					<div>
-						<p>
-							Edit <span className="code">main.tsx</span> and save to test HMR
-							updates.
-						</p>
-					</div>
-					<div className="button-group">
-						<a href="https://github.com/hyperbrew/bolt-uxp/">Bolt UXP Docs</a>
-						<a href="https://svelte.dev">Svelte Docs</a>
-						<a href="https://vitejs.dev/">Vite Docs</a>
-					</div>
-				</main>
-			) : (
-				<></>
-			)}
+		<div>
+			<button onClick={handleClick}>click me! {count}</button>
 
-			{/* Example of a secondary panel entrypoint 
-      <uxp-panel panelid="bolt.uxp.plugin.settings">
-        <h1>Settings Panel</h1>
-        <p>count is: {count}</p>
-      </uxp-panel>
-      */}
-		</>
+			<label htmlFor="opacity-checkbox">include opacity?</label>
+			<input id="opacity-checkbox" type="checkbox" checked></input>
+		</div>
 	);
+	// const webviewUI = import.meta.env.VITE_BOLT_WEBVIEW_UI === "true";
+
+	// const [count, setCount] = useState(0);
+	// const increment = () => setCount((prev) => prev + 1);
+
+	// const hostName = (uxp.host.name as string).toLowerCase();
+
+	// //* Call Functions Conditionally by App
+	// if (hostName === "premierepro") {
+	// 	console.log("Hello from Premiere Pro", premierepro);
+	// }
+
+	// * Or call the unified API object directly and the correct app function will be used
+	// const simpleAlert = () => {
+	// 	api.notify("Hello World");
+	// };
+	// return (
+	// 	<>
+	// 		{!webviewUI ? (
+	// 			<main>
+	// 				<div>
+	// 					<img className="logo-lg" src={boltUxpLogo} alt="" />
+	// 				</div>
+	// 				<div className="stack-icons">
+	// 					<img src={viteLogo} className="logo" alt="" />
+	// 					<span> + </span>
+	// 					<img src={reactLogo} className="logo" alt="" />
+	// 					<span> + </span>
+	// 					<img src={tsLogo} className="logo" alt="" />
+	// 					<span> + </span>
+	// 					<img src={sassLogo} className="logo" alt="" />
+	// 				</div>
+	// 				<div className="button-group">
+	// 					<button onClick={increment}>count is {count}</button>
+	// 					<button onClick={simpleAlert}>Alert</button>
+	// 				</div>
+	// 				<div>
+	// 					<p>
+	// 						Edit <span className="code">main.tsx</span> and save to test HMR
+	// 						updates.
+	// 					</p>
+	// 				</div>
+	// 				<div className="button-group">
+	// 					<a href="https://github.com/hyperbrew/bolt-uxp/">Bolt UXP Docs</a>
+	// 					<a href="https://svelte.dev">Svelte Docs</a>
+	// 					<a href="https://vitejs.dev/">Vite Docs</a>
+	// 				</div>
+	// 			</main>
+	// 		) : (
+	// 			<></>
+	// 		)}
+
+	// 		{/* Example of a secondary panel entrypoint
+	//     <uxp-panel panelid="bolt.uxp.plugin.settings">
+	//       <h1>Settings Panel</h1>
+	//       <p>count is: {count}</p>
+	//     </uxp-panel>
+	//     */}
+	// 	</>
+	// );
 };
---
+++ src/types/effects-params.ts
@@ -0,0 +1,13 @@
+export type MotionParams = {
+	position: number;
+	scale: number;
+	scaleWidth: number;
+	uniformScale: number;
+	rotation: number;
+	anchorPoint: number;
+	antiFlickerFilter: number;
+	cropLeft: number;
+	cropTop: number;
+	cropRight: number;
+	cropBottom: number;
+};
